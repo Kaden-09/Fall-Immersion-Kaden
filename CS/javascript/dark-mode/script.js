8fb1d47aad8858c7b09c0a65b5fcf54d3cd0c408
Kaden-09 <kaden.whitley@hgs.hiddengeniusproject.org>
2024-10-02 17:44:20 -0700

    Update script.js

--- CS/javascript/dark-mode/script.js
+++ CS/javascript/dark-mode/script.js
@@ -1,3 +1,13 @@
 function darkMode() {
-// Your code here.
+body {
+    padding: 25px;
+    background-color: white;
+    color: black;
+    font-size: 25px;
+  }
+  
+  .dark-mode {
+    background-color: black;
+    color: white;
+  }
 }
